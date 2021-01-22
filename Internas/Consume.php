04782808780a54cf6f2db723a07628481bef2e27
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2021-01-21 21:12:42 -0500

    Delete Internas directory

--- Internas/Consume.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = json_decode(file_get_contents("https://api-easylab.herokuapp.com/ApiRest/Formulario?fbclid=IwAR0O6juItrqp2UJfWoD4tUEQueiyxmcvVLwLCr-FQfGgXFfFsgR8H4c8gXo"), true);
-echo "<br>";
-
-for ($i=0; $i < count($data); $i++) { 
-	echo $data[$i]["nombre_Pregunta"]."<br>";
-}
-?>
